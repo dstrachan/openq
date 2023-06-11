53fe2a5b59849f57b9c1f93992e927070762a973
David Strachan <davidstrachan92@gmail.com>
2023-06-11 10:41:55 +0100

    Remove calculated list tokens

--- src/tests/scanner/guid.zig
+++ src/tests/scanner/guid.zig
@@ -6,10 +6,3 @@ test "guid tokens" {
     try testTokens("0Ng", .{.guid});
     try testTokens("0ng", .{.guid});
 }
-
-test "guid_list tokens" {
-    try testTokens("0N 0Ng", .{.guid_list});
-    try testTokens("0N 0ng", .{.guid_list});
-    try testTokens("0n 0Ng", .{.guid_list});
-    try testTokens("0n 0ng", .{.guid_list});
-}
