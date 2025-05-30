fa71e8657f5f9f167beaa56a0007905f8ee548c8
David Strachan <david@kdblint.com>
2025-05-30 19:05:07 +0100

    Implement JSON writer node tags

--- src/utils.zig
+++ src/utils.zig
@@ -20,83 +20,206 @@ pub fn writeJsonNode(writer: AnyWriter, tree: Ast, node: Node.Index) !void {
                 if (i < statements.len - 1) try writer.writeByte(',');
             }
 
+            try writer.writeAll("]}\n");
+        },
+        .no_op => try writer.writeAll(
+            \\{"no_op":""}
+        ),
+
+        .grouped_expression => {
+            try writer.writeAll(
+                \\{"grouped_expression":
+            );
+
+            try writeJsonNode(writer, tree, tree.nodeData(node).node_and_token[0]);
+
+            try writer.writeAll("}");
+        },
+
+        .empty_list => try writer.writeAll(
+            \\{"empty_list":[]}
+        ),
+        inline .list, .expr_block => |t| {
+            try writer.writeAll("{\"" ++ @tagName(t) ++ "\":[");
+
+            const nodes = tree.extraDataSlice(tree.nodeData(node).extra_range, Node.Index);
+
+            for (nodes, 0..) |n, i| {
+                try writeJsonNode(writer, tree, n);
+                if (i < nodes.len - 1) try writer.writeByte(',');
+            }
+
             try writer.writeAll("]}");
         },
-        .no_op => unreachable,
 
-        .grouped_expression => unreachable,
+        .table_literal => {
+            try writer.writeAll(
+                \\{"table_literal":{"keys":[
+            );
 
-        .empty_list => unreachable,
-        .list => unreachable,
+            const table = tree.extraData(tree.nodeData(node).extra_and_token[0], Node.Table);
 
-        .table_literal => unreachable,
+            const keys = tree.extraDataSlice(.{
+                .start = table.keys_start,
+                .end = table.columns_start,
+            }, Node.Index);
+            for (keys, 0..) |n, i| {
+                try writeJsonNode(writer, tree, n);
+                if (i < keys.len - 1) try writer.writeByte(',');
+            }
 
-        .function => unreachable,
+            try writer.writeAll(
+                \\],"columns":[
+            );
 
-        .expr_block => unreachable,
+            const columns = tree.extraDataSlice(.{
+                .start = table.columns_start,
+                .end = table.columns_end,
+            }, Node.Index);
+            for (columns, 0..) |n, i| {
+                try writeJsonNode(writer, tree, n);
+                if (i < columns.len - 1) try writer.writeByte(',');
+            }
 
-        .colon => unreachable,
-        .colon_colon => unreachable,
-        .plus => try writer.print(
-            \\{{"{s}":"{s}"}}
-        , .{ @tagName(tree.nodeTag(node)), tree.tokenSlice(tree.nodeMainToken(node)) }),
-        .plus_colon => unreachable,
-        .minus => unreachable,
-        .minus_colon => unreachable,
-        .asterisk => try writer.print(
-            \\{{"{s}":"{s}"}}
-        , .{ @tagName(tree.nodeTag(node)), tree.tokenSlice(tree.nodeMainToken(node)) }),
-        .asterisk_colon => unreachable,
-        .percent => unreachable,
-        .percent_colon => unreachable,
-        .ampersand => unreachable,
-        .ampersand_colon => unreachable,
-        .pipe => unreachable,
-        .pipe_colon => unreachable,
-        .caret => unreachable,
-        .caret_colon => unreachable,
-        .equal => unreachable,
-        .equal_colon => unreachable,
-        .l_angle_bracket => unreachable,
-        .l_angle_bracket_colon => unreachable,
-        .l_angle_bracket_equal => unreachable,
-        .l_angle_bracket_r_angle_bracket => unreachable,
-        .r_angle_bracket => unreachable,
-        .r_angle_bracket_colon => unreachable,
-        .r_angle_bracket_equal => unreachable,
-        .dollar => unreachable,
-        .dollar_colon => unreachable,
-        .comma => unreachable,
-        .comma_colon => unreachable,
-        .hash => unreachable,
-        .hash_colon => unreachable,
-        .underscore => unreachable,
-        .underscore_colon => unreachable,
-        .tilde => unreachable,
-        .tilde_colon => unreachable,
-        .bang => unreachable,
-        .bang_colon => unreachable,
-        .question_mark => unreachable,
-        .question_mark_colon => unreachable,
-        .at => unreachable,
-        .at_colon => unreachable,
-        .dot => unreachable,
-        .dot_colon => unreachable,
-        .zero_colon => unreachable,
-        .zero_colon_colon => unreachable,
-        .one_colon => unreachable,
-        .one_colon_colon => unreachable,
-        .two_colon => unreachable,
+            try writer.writeAll("]}}");
+        },
 
-        .apostrophe => unreachable,
-        .apostrophe_colon => unreachable,
-        .slash => unreachable,
-        .slash_colon => unreachable,
-        .backslash => unreachable,
-        .backslash_colon => unreachable,
+        .function => {
+            try writer.writeAll(
+                \\{"function":{"params":[
+            );
 
-        .call => unreachable,
-        .apply_unary => unreachable,
+            const function = tree.extraData(tree.nodeData(node).extra_and_token[0], Node.Function);
+
+            const params = tree.extraDataSlice(.{
+                .start = function.params_start,
+                .end = function.body_start,
+            }, Node.Index);
+            for (params, 0..) |n, i| {
+                try writeJsonNode(writer, tree, n);
+                if (i < params.len - 1) try writer.writeByte(',');
+            }
+
+            try writer.writeAll(
+                \\],"body":[
+            );
+
+            const body = tree.extraDataSlice(.{
+                .start = function.body_start,
+                .end = function.body_end,
+            }, Node.Index);
+            for (body, 0..) |n, i| {
+                try writeJsonNode(writer, tree, n);
+                if (i < body.len - 1) try writer.writeByte(',');
+            }
+
+            try writer.writeAll("]}}");
+        },
+
+        inline .colon,
+        .colon_colon,
+        .plus,
+        .plus_colon,
+        .minus,
+        .minus_colon,
+        .asterisk,
+        .asterisk_colon,
+        .percent,
+        .percent_colon,
+        .ampersand,
+        .ampersand_colon,
+        .pipe,
+        .pipe_colon,
+        .caret,
+        .caret_colon,
+        .equal,
+        .equal_colon,
+        .l_angle_bracket,
+        .l_angle_bracket_colon,
+        .l_angle_bracket_equal,
+        .l_angle_bracket_r_angle_bracket,
+        .r_angle_bracket,
+        .r_angle_bracket_colon,
+        .r_angle_bracket_equal,
+        .dollar,
+        .dollar_colon,
+        .comma,
+        .comma_colon,
+        .hash,
+        .hash_colon,
+        .underscore,
+        .underscore_colon,
+        .tilde,
+        .tilde_colon,
+        .bang,
+        .bang_colon,
+        .question_mark,
+        .question_mark_colon,
+        .at,
+        .at_colon,
+        .dot,
+        .dot_colon,
+        .zero_colon,
+        .zero_colon_colon,
+        .one_colon,
+        .one_colon_colon,
+        .two_colon,
+        => |t| try writer.print(
+            "{{\"" ++ @tagName(t) ++ "\":\"{s}\"}}",
+            .{tree.tokenSlice(tree.nodeMainToken(node))},
+        ),
+
+        inline .apostrophe,
+        .apostrophe_colon,
+        .slash,
+        .slash_colon,
+        .backslash,
+        .backslash_colon,
+        => |t| {
+            try writer.writeAll("{\"" ++ @tagName(t) ++ "\":");
+
+            const maybe_lhs = tree.nodeData(node).opt_node;
+            if (maybe_lhs.unwrap()) |lhs| {
+                try writeJsonNode(writer, tree, lhs);
+            } else {
+                try writer.writeAll("{}");
+            }
+
+            try writer.writeByte('}');
+        },
+
+        .call => {
+            const nodes = tree.extraDataSlice(tree.nodeData(node).extra_range, Node.Index);
+            assert(nodes.len > 0);
+
+            try writer.writeAll(
+                \\{"call":{"function":
+            );
+
+            try writeJsonNode(writer, tree, nodes[0]);
+            try writer.writeAll(
+                \\,"args":[
+            );
+            for (nodes[1..], 1..) |n, i| {
+                try writeJsonNode(writer, tree, n);
+                if (i < nodes.len - 1) try writer.writeByte(',');
+            }
+
+            try writer.writeAll("]}}");
+        },
+        .apply_unary => {
+            const lhs, const rhs = tree.nodeData(node).node_and_node;
+
+            try writer.writeAll(
+                \\{"apply_unary":[
+            );
+
+            try writeJsonNode(writer, tree, lhs);
+            try writer.writeByte(',');
+            try writeJsonNode(writer, tree, rhs);
+
+            try writer.writeAll("]}");
+        },
         .apply_binary => {
             const lhs, const maybe_rhs = tree.nodeData(node).node_and_opt_node;
             const op: Node.Index = @enumFromInt(tree.nodeMainToken(node));
@@ -115,22 +238,83 @@ pub fn writeJsonNode(writer: AnyWriter, tree: Ast, node: Node.Index) !void {
             try writer.writeAll("]}");
         },
 
-        .number_literal => {
-            try writer.print(
-                \\{{"number_literal":"{s}"}}
-            , .{tree.tokenSlice(tree.nodeMainToken(node))});
-        },
-        .number_list_literal => unreachable,
-        .string_literal => unreachable,
-        .symbol_literal => unreachable,
-        .symbol_list_literal => unreachable,
-        .identifier => unreachable,
-        .builtin => unreachable,
+        inline .number_literal,
+        .symbol_literal,
+        .identifier,
+        .builtin,
+        => |t| try writer.print(
+            "{{\"" ++ @tagName(t) ++ "\":\"{s}\"}}",
+            .{tree.tokenSlice(tree.nodeMainToken(node))},
+        ),
+        .string_literal,
+        => try writer.print(
+            \\{{"string_literal":"{}"}}
+        , .{std.zig.fmtEscapes(tree.tokenSlice(tree.nodeMainToken(node)))}),
+        inline .number_list_literal,
+        .symbol_list_literal,
+        => |t| {
+            try writer.writeAll("{\"" ++ @tagName(t) ++ "\":[");
 
-        .select => unreachable,
-        .exec => unreachable,
-        .update => unreachable,
-        .delete_rows => unreachable,
-        .delete_cols => unreachable,
+            const first_token = tree.nodeMainToken(node);
+            const last_token = tree.nodeData(node).token;
+            for (first_token..last_token + 1) |tok_i| {
+                try writer.print("\"{s}\"", .{tree.tokenSlice(@intCast(tok_i))});
+                if (tok_i < last_token) try writer.writeByte(',');
+            }
+
+            try writer.writeAll("]}");
+        },
+
+        .select => {
+            try writer.writeAll(
+                \\{"select":{"select":[
+            );
+
+            const select = tree.extraData(tree.nodeData(node).extra, Node.Select);
+
+            const select_nodes = tree.extraDataSlice(.{
+                .start = select.select_start,
+                .end = select.by_start,
+            }, Node.Index);
+            for (select_nodes, 0..) |n, i| {
+                try writeJsonNode(writer, tree, n);
+                if (i < select_nodes.len - 1) try writer.writeByte(',');
+            }
+
+            try writer.writeAll(
+                \\],"by":[
+            );
+            const by_nodes = tree.extraDataSlice(.{
+                .start = select.by_start,
+                .end = select.where_start,
+            }, Node.Index);
+            for (by_nodes, 0..) |n, i| {
+                try writeJsonNode(writer, tree, n);
+                if (i < by_nodes.len - 1) try writer.writeByte(',');
+            }
+
+            try writer.writeAll(
+                \\],"from":
+            );
+            try writeJsonNode(writer, tree, select.from);
+
+            try writer.writeAll(
+                \\,"where":[
+            );
+            const where_nodes = tree.extraDataSlice(.{
+                .start = select.where_start,
+                .end = select.where_end,
+            }, Node.Index);
+            for (where_nodes, 0..) |n, i| {
+                try writeJsonNode(writer, tree, n);
+                if (i < where_nodes.len - 1) try writer.writeByte(',');
+            }
+
+            try writer.writeAll("]}}");
+        },
+        .exec => @panic("NYI"),
+        .update => @panic("NYI"),
+        .delete_rows => @panic("NYI"),
+        .delete_cols => @panic("NYI"),
     }
 }
