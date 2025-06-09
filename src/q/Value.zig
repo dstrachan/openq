79710b79e9a8bac7b0665522f27a6cf9e4641905
David Strachan <david@kdblint.com>
2025-06-09 22:15:41 +0100

    Add list types

--- src/q/Value.zig
+++ src/q/Value.zig
@@ -4,31 +4,52 @@ const assert = std.debug.assert;
 
 const Value = @This();
 
-pub const ValueType = enum(u8) {
+pub const ValueType = enum {
+    mixed_list,
     boolean,
+    boolean_list,
     guid,
+    guid_list,
     byte,
+    byte_list,
     short,
+    short_list,
     int,
+    int_list,
     long,
+    long_list,
     real,
+    real_list,
     float,
+    float_list,
     char,
+    char_list,
     symbol,
+    symbol_list,
 };
 
 pub const ValueUnion = union {
+    mixed_list: []Value,
     boolean: bool,
+    boolean_list: []bool,
     guid: [16]u8,
+    guid_list: [][16]u8,
     byte: u8,
+    byte_list: []u8,
     short: i16,
+    short_list: []i16,
     int: i32,
+    int_list: []i32,
     long: i64,
+    long_list: []i64,
     real: f32,
+    real_list: []f32,
     float: f64,
+    float_list: []f64,
+    char: u8,
+    char_list: []u8,
     symbol: [*:0]u8,
-    table: [*]Value,
-    list: []u8,
+    symbol_list: [][*:0]u8,
 };
 
 type: ValueType,
@@ -91,15 +112,39 @@ pub fn deref(value: *Value) void {
 pub fn print(value: Value) !void {
     const stdout = std.io.getStdOut().writer();
     switch (value.type) {
+        .mixed_list => @panic("NYI"),
         .boolean => try stdout.writeAll(if (value.as.boolean) "1b" else "0b"),
+        .boolean_list => if (value.as.boolean_list.len == 0) {
+            try stdout.writeAll("`boolean$()");
+        } else {
+            for (value.as.boolean_list) |b| try stdout.writeByte(if (b) '1' else '0');
+            try stdout.writeByte('b');
+        },
         .guid => @panic("NYI"),
+        .guid_list => @panic("NYI"),
         .byte => try stdout.print("0x{d}", .{std.fmt.fmtSliceHexLower(&.{value.as.byte})}),
+        .byte_list => if (value.as.byte_list.len == 0) {
+            try stdout.writeAll("`byte$()");
+        } else {
+            try stdout.print("0x{d}", .{std.fmt.fmtSliceHexLower(value.as.byte_list)});
+        },
         .short => try stdout.print("{d}h", .{value.as.short}),
+        .short_list => @panic("NYI"),
         .int => try stdout.print("{d}i", .{value.as.int}),
+        .int_list => @panic("NYI"),
         .long => try stdout.print("{d}", .{value.as.long}),
+        .long_list => @panic("NYI"),
         .real => try stdout.print("{d}e", .{value.as.real}),
+        .real_list => @panic("NYI"),
         .float => try stdout.print("{d}f", .{value.as.float}),
-        .char => try stdout.print("\"{c}\"", .{value.as.byte}),
+        .float_list => @panic("NYI"),
+        .char => try stdout.print("\"{}\"", .{std.zig.fmtEscapes(&.{value.as.char})}),
+        .char_list => if (value.as.char_list.len == 1) {
+            try stdout.print(",\"{}\"", .{std.zig.fmtEscapes(value.as.char_list)});
+        } else {
+            try stdout.print("\"{}\"", .{std.zig.fmtEscapes(value.as.char_list)});
+        },
         .symbol => try stdout.print("`{s}", .{value.as.symbol}),
+        .symbol_list => @panic("NYI"),
     }
 }
